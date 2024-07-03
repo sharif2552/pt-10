eadd78547cabe2d7a108e4036a64969df9562d37
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-07-04 00:12:59 +0600

    chore: Update PartnersSection component to remove unused partner4 image, update styles and image sizes, update Navbar component styles, add lazy loading for logo image, improve mobile menu functionality, update Timeline component styles and add hover effect to timeline content, update HeroSection link to documentation, update SocialProofSection to use lazy loading for video thumbnails, and update TeamSection component to use FaDiscord icon instead of FiDisc

--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -11,18 +11,15 @@ const Timeline = () => {
   return (
     <div className="dark:bg-[#14102C]">
       <section className="design-section">
-        <h1 className="text-center text-black dark:text-white text-7xl mb-5 text-green-600">
-          RoadMap
-        </h1>
-
         {/* Year 2024 */}
         <div className="year2024">
           <div className="cursor-pointer" onClick={() => toggleYear(2024)}>
-            <h2 className="text-black dark:text-white text-6xl mb-5 mt-10 text-blue-800">2024</h2>
+            <h2 className="text-black dark:text-white text-6xl mb-5 mt-10 text-blue-800">
+              RoadMap
+            </h2>
           </div>
           {visibleYear === 2024 && (
             <div className="q1Timeline">
-              <h3 className="text-black dark:text-white text-4xl mb-5">Q1</h3>
               <div className="timeline">
                 {/* First row */}
                 <div className="timeline-empty"></div>
@@ -30,12 +27,26 @@ const Timeline = () => {
                   <div className="timeline-circle"></div>
                 </div>
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">Mynth Liquid Staking</h3>
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Mynth Liquid Staking
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    Mynth offers Liquid Staking for Chameleon, MNT, and MyUSD
+                    holders, earning compounded rewards every five days (epoch).
+                    All with full assets custody and no locking.
+                  </p>
                 </div>
 
                 {/* Second row */}
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">MyUSD {">"} ADA</h3>
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Mynth Streaming Swaps
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    Streaming Swaps break orders into smaller portions to avoid
+                    excess fees or slippage. This allows you to trade larger
+                    amounts of MyUSD worry-free.
+                  </p>
                 </div>
                 <div className="timeline-middle">
                   <div className="timeline-circle"></div>
@@ -48,12 +59,25 @@ const Timeline = () => {
                   <div className="timeline-circle"></div>
                 </div>
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">ADA {">"} MyUSD</h3>
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Cardano and Polygon Integration
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    Mynth is integrating Cardano with Polygon to enable direct
+                    swaps between tokens on both networks, creating a more
+                    interconnected cryptocurrency ecosystem.
+                  </p>
                 </div>
 
                 {/* Fourth row */}
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">Polygon Integration</h3>
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    USDC Available on Cardano
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    Adding a 1:1 swap option for USDC with MyUSD and enhancing
+                    stablecoin interoperability.
+                  </p>
                 </div>
                 <div className="timeline-middle">
                   <div className="timeline-circle"></div>
@@ -66,196 +90,113 @@ const Timeline = () => {
                   <div className="timeline-circle"></div>
                 </div>
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">USDC {">"} MyUSD</h3>
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Cardano and Solana
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    Integrating Cardano with Solana to allow direct exchanges
+                    between tokens on both networks.
+                  </p>
                 </div>
 
                 {/* Sixth row */}
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">MyUSD {">"} USDC</h3>
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Mynth CNT Swaps
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    We're making all Cardano Native Token exchanges seamless,
+                    leveraging Cardano's decentralization.
+                  </p>
                 </div>
                 <div className="timeline-middle">
                   <div className="timeline-circle"></div>
                 </div>
                 <div className="timeline-empty"></div>
               </div>
-            </div>
-          )}
 
-          {/* Q2 timeline */}
-          {visibleYear === 2024 && (
-            <div className="q2Timeline">
-              <h3 className="text-black dark:text-white text-4xl my-5">Q2</h3>
               <div className="timeline">
-                {/* First row */}
+                {/* Seventh row */}
                 <div className="timeline-empty"></div>
                 <div className="timeline-middle">
                   <div className="timeline-circle"></div>
                 </div>
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  Solana Integration
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Mynth DAO
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    Governed by the MNT token, the DAO allows members to
+                    influence the protocol’s direction by voting on key
+                    initiatives, contributing to Mynth's future.
+                  </p>
                 </div>
 
-                {/* Second row */}
+                {/* Eighth row */}
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">CNT {">"} USDT</h3>
-                </div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-empty"></div>
-
-                {/* Third row */}
-                <div className="timeline-empty"></div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">USDT {">"} CNT</h3>
-                </div>
-
-                {/* Fourth row */}
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">CNT {">"} USDT</h3>
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Cardano and Arbitrum Integration
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    We're linking Cardano with Arbitrum fostering a more
+                    integrated cryptocurrency ecosystem.
+                  </p>
                 </div>
                 <div className="timeline-middle">
                   <div className="timeline-circle"></div>
                 </div>
                 <div className="timeline-empty"></div>
 
-                {/* Fifth row */}
+                {/* Ninth row */}
+                <div className="timeline-empty"></div>
+                <div class="timeline-middle">
+                  <div className="timeline-circle"></div>
+                </div>
+                <div className="timeline-component timeline-content dark:timeline-content-dark">
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Cardano and Bitcoin Integration
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    This integration combines Bitcoin's security and market
+                    presence with Cardano's advanced programmability, enhancing
+                    the cryptocurrency environment.
+                  </p>
+                </div>
+
+                {/* Tenth row */}
+                <div className="timeline-component timeline-content dark:timeline-content-dark">
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Mynth Savings Account
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    Mynth Savings Account is a modern way to earn more rewards.
+                    You maintain control of your funds at all times, with no
+                    penalties or locking.
+                  </p>
+                </div>
+                <div className="timeline-middle">
+                  <div className="timeline-circle"></div>
+                </div>
+                <div className="timeline-empty"></div>
+
+                {/* Eleventh row */}
                 <div className="timeline-empty"></div>
                 <div className="timeline-middle">
                   <div className="timeline-circle"></div>
                 </div>
                 <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">Mynth DAO Voting</h3>
+                  <h3 className="text-white mb-2 text-2xl font-bold">
+                    Mynth Stable Insurance
+                  </h3>
+                  <p className="text-gray-300 text-lg">
+                    We aim to protect MyUSD holders from unforeseen events.
+                    Stables Insurance reflects our dedication to fortifying
+                    MyUSD's robustness and providing you with peace of mind.
+                  </p>
                 </div>
               </div>
             </div>
           )}
-
-          {/* Q3 timeline */}
-          {visibleYear === 2024 && (
-            <div className="q3Timeline">
-              <h3 className="text-black dark:text-white text-4xl my-5">Q3</h3>
-              <div className="timeline">
-                {/* First row */}
-                <div className="timeline-empty"></div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  Arbitrum Integration
-                </div>
-
-                {/* Second row */}
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">Bitcoin Integration</h3>
-                </div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-empty"></div>
-              </div>
-            </div>
-          )}
-
-          {/* Q4 timeline */}
-          {visibleYear === 2024 && (
-            <div className="q4Timeline">
-              <h3 className="text-black dark:text-white text-4xl my-5">Q4</h3>
-              <div className="timeline">
-                {/* First row */}
-                <div className="timeline-empty"></div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  Mynth Savings Account
-                </div>
-
-                {/* Second row */}
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">Mynth Stable Insurance</h3>
-                </div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-empty"></div>
-              </div>
-            </div>
-          )}
-        </div>
-
-        {/* Year 2023 */}
-        <div className="year2023">
-          <div className="cursor-pointer" onClick={() => toggleYear(2023)}>
-            <h2 className="text-black dark:text-white text-6xl mt-10 mb-5 text-blue-800">
-              2023
-            </h2>
-          </div>
-          {visibleYear === 2023 && (
-            <div className="q1Timeline_2">
-              <h3 className="text-black dark:text-white text-4xl mb-5">Q4</h3>
-              <div className="timeline">
-                {/* First row */}
-                <div className="timeline-empty"></div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">iUSD {">"} USDT</h3>
-                </div>
-
-                {/* Second row */}
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">Babel Fees</h3>
-                </div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-empty"></div>
-
-                {/* Third row */}
-                <div className="timeline-empty"></div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">Mynth Genesis</h3>
-                </div>
-
-                {/* Fourth row */}
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">Mynth Stablecoin</h3>
-                </div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-empty"></div>
-
-                {/* Fifth row */}
-                <div className="timeline-empty"></div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">MyUSD {">"} USDT</h3>
-                </div>
-
-                {/* Sixth row */}
-
-                <div className="timeline-component timeline-content dark:timeline-content-dark">
-                  <h3 className="text-white">USDT {">"} MyUSD</h3>
-                </div>
-                <div className="timeline-middle">
-                  <div className="timeline-circle"></div>
-                </div>
-                <div className="timeline-empty"></div>
-              </div>
-            </div>
-          )}
-
         </div>
       </section>
     </div>
