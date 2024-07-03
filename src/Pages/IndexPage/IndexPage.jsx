3a86cefc5a08bc859bdccbbde331c81bdbe11fd3
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2024-07-03 13:09:04 +0600

    chore: Add react-intersection-observer package for lazy loading images on IndexPage

--- src/Pages/IndexPage/IndexPage.jsx
+++ src/Pages/IndexPage/IndexPage.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import "./IndexPage.css";
+
 import logo from "../../Assets/img/logo.svg";
 import technologyDark from "../../Assets/img/technology_dark.png";
 import technologyLight from "../../Assets/img/technology_light.png";
@@ -21,6 +23,19 @@ const IndexPage = () => {
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
+  const sectionVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+
+  const [ref, inView] = useInView({ triggerOnce: true });
+
+  useEffect(() => {
+    if (inView) {
+      document.getElementById("myUSDSection").classList.add("animate");
+    }
+  }, [inView]);
+
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
@@ -60,7 +75,7 @@ const IndexPage = () => {
             style={{ "--delay": "-10s", "--size": "0.3", "--speed": "15s" }}
           ></div>
         </div>
-        <div className=" mx-5 z-40">
+        <div className="mx-5 z-40">
           <motion.h1
             className="text-6xl font-bold mb-4 z-50"
             initial={{ opacity: 0, y: -50 }}
@@ -100,11 +115,27 @@ const IndexPage = () => {
       </section>
 
       {/* MyUSD Introduction Section */}
-      <div className="flex w-100vw flex-row justify-center">
-        <section className="flex flex-col w-full lg:flex-row lg:justify-center items-center lg:items-start py-20 lg:px-32 bg-white dark:bg-custom-secondary-dark text-center lg:text-left lg:space-x-10 space-y-10 lg:space-y-0 px-4 lg:flex">
+      <div className="flex w-full flex-row justify-center">
+        <section
+          ref={ref}
+          id="myUSDSection"
+          className="flex flex-col w-full lg:flex-row lg:justify-center items-center lg:items-start py-20 lg:px-32 bg-white dark:bg-custom-secondary-dark text-center lg:text-left lg:space-x-10 space-y-10 lg:space-y-0 px-4 lg:flex"
+        >
           <div className="lg:w-1/2 flex flex-col justify-center">
-            <h2 className="text-4xl font-bold mb-4">MyUSD</h2>
-            <p className="text-xl text-gray-700 dark:text-gray-300 max-w-2xl mx-auto lg:mx-0">
+            <motion.h2
+              className="text-4xl font-bold mb-4"
+              initial={{ opacity: 0, y: 50 }}
+              animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+              transition={{ duration: 1 }}
+            >
+              MyUSD
+            </motion.h2>
+            <motion.p
+              className="text-xl text-gray-700 dark:text-gray-300 max-w-2xl mx-auto lg:mx-0"
+              initial={{ opacity: 0, y: 50 }}
+              animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+              transition={{ duration: 1, delay: 0.5 }}
+            >
               MyUSD is a stablecoin designed to provide a stable, scalable, and
               fully redeemable option within the Cardano ecosystem. It is pegged
               to the US dollar, ensuring that its value remains consistent and
@@ -112,10 +143,15 @@ const IndexPage = () => {
               broader mission to create a seamless and integrated DeFi
               experience, ensuring users can easily transfer, exchange, and
               manage their digital assets across various blockchain platforms.
-            </p>
+            </motion.p>
           </div>
           <div className="lg:w-96 lg:h-full flex flex-col justify-center">
-            <div className="relative w-full h-52 lg:max-w-screen-md">
+            <motion.div
+              className="relative w-full h-52 lg:max-w-screen-md"
+              initial={{ opacity: 0, y: 50 }}
+              animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+              transition={{ duration: 1, delay: 1 }}
+            >
               <div className="flex justify-center w-full h-full">
                 <div className="w-full aspect-w-16 aspect-h-9 rounded-lg shadow-lg overflow-hidden">
                   <iframe
@@ -129,7 +165,7 @@ const IndexPage = () => {
                   ></iframe>
                 </div>
               </div>
-            </div>
+            </motion.div>
           </div>
         </section>
       </div>
